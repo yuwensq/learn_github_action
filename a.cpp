38c2c59682346b7bdc8c040c93b05fb0ce069ad4
yuwensq <2577123627@qq.com>
2023-03-07 22:36:49 +0800

    a

---
+++ a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main() {
+    srand(time(0));
+    for (int i = 0; i < 10; i++) {
+        cout << rand() % 522 << " ";
+    }
+    return 0;
+}
